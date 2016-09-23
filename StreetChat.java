b943dc20e6b3e151fad91e8c0904e43df3e29aa4
AMUDA Adeolu Badmus <badmusamuda@gmail.com>
2016-09-23 12:23:50 +0100

    Update StreetChat.java

--- StreetChat.java
+++ StreetChat.java
@@ -1,16 +1,10 @@
-
 import java.io.*;
-
 public class StreetChat implements Serializable {
-
- 
-
-    protected static final long streetVersion = 10102100L;
-
+ protected static final long streetVersion = 10102100L;
     static final int WHOIDEYSTREET = 0, MESSAGE = 1, LOGOUT = 2;
-
     private int type;
-
+    private double streetLat;
+    private double streetLong;
     private String message;
 
      
